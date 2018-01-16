a5b1a352233b6b2491dafe2b736ee4a2b2f8e5a6
admin <admin@admin-PC>
2018-01-17 06:34:48 +0800

    projek5

---
+++ src/gred.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author haja
+ */
+import java.util.Scanner;
+public class gred {
+public static void main(String[] args) {
+    Scanner input = new Scanner (System.in);
+     char sains;
+     char math;
+    
+     System.out.println("Apakah gred bagi Math:[A/B/C/D/E/F]");
+        sains=input.next().charAt(0);
+     System.out.println("Apakah gred bagi Sains:[A/B/C/D/E/F]");
+        math=input.next().charAt(0);
+        
+    if (sains==math)
+                System.out.println("Gred yang diperolehi sama");
+    if (sains!=math) // also CORRECT>
+                System.out.println("Gred yang diperolehi tidak sama");
+    } 
+}
+
+
